f2da58a6e02456a94f66089a836aa2274beba381
Jason Antman <jason@jasonantman.com>
2015-09-08 23:00:19 -0400

    fix missing jantman.db

--- update_sync.sh
+++ update_sync.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 repo-add repo/jantman.db.tar.gz repo/*.pkg.tar.xz
-~/venvs/foo/bin/s3cmd put -r repo/* s3://archrepo.jasonantman.com/current/
+~/venvs/foo/bin/s3cmd sync repo/* s3://archrepo.jasonantman.com/current/
+# local symlink
+~/venvs/foo/bin/s3cmd put repo/jantman.db.tar.gz s3://archrepo.jasonantman.com/current/jantman.db
