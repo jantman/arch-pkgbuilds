077740de0147f49756151af9fcb14c30cac704d9
Jason Antman <jason@jasonantman.com>
2016-02-10 21:17:36 -0500

    add aurget script - maybe I should just give up and use submodules

---
+++ aurget.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+# aurget - pull down an AUR package and add it to my git repo
+
+packagename=$1
+dest_dir=$packagename
+[[ -d $dest_dir ]] && rm -Rf $dest_dir
+mkdir $dest_dir
+git clone --depth=1 "https://aur.archlinux.org/${packagename}.git" $dest_dir
+pushd $dest_dir
+gitrev=$(git rev-parse HEAD)
+read -p "Opening PKGBUILD with less. Please inspect for any malicious code."
+less PKGBUILD
+read -p "Ctrl+C now to exit, or continue to source PKGBUILD and get vars"
+pkgver=`source PKGBUILD && echo "\${pkgver}-\${pkgrel}"`
+rm -Rf .git
+popd
+git add $dest_dir
+git commit -m "aurget.sh - pull in ${packagename}-${pkgver} from AUR at commit ${gitrev}"
