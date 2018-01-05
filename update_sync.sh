41d1fd57346f5e1bb073f16b54fb316625f395a4
Jason Antman <jason@jasonantman.com>
2018-01-05 14:53:53 -0500

    fix s3 upload

--- update_sync.sh
+++ update_sync.sh
@@ -3,8 +3,8 @@
 repo-add -n repo/jantman.db.tar.gz repo/*.pkg.tar.xz
 source ~/venvs/current/bin/activate
 python arch_repo_index.py -vv -b http://archrepo.jasonantman.com/current/ -o repo/index.html repo/jantman.db.tar.gz
-~/venvs/foo/bin/s3cmd sync repo/* s3://archrepo.jasonantman.com/current/
+~/bin/aws s3 sync repo s3://archrepo.jasonantman.com/current/ --delete
 # local symlink
-~/venvs/foo/bin/s3cmd put repo/jantman.db.tar.gz s3://archrepo.jasonantman.com/current/jantman.db
+~/bin/aws s3 cp repo/jantman.db.tar.gz s3://archrepo.jasonantman.com/current/jantman.db
 
 echo "WARNING: because Arch doesn't really support versioning, you can only have one version of a given package in a repo at a time. 'repo-add' will remove the old version before adding a new one. Unfortunately, it doesn't seem to have any version comparison logic; using '*' here will add packages in lexicographic order, so if we have both foo-1.2.2 and foo-1.2.12 in the source directory, 'foo-1.2.2' will win. Yuck."
