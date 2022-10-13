bb62d10228dfdf22f01c799ce03d28af90436671
Jason Antman <jason@jasonantman.com>
2022-10-13 15:27:23 -0400

    update

--- rebuild.py
+++ rebuild.py
@@ -20,7 +20,9 @@ FORMAT = "[%(asctime)s %(levelname)s] %(message)s"
 logging.basicConfig(level=logging.WARNING, format=FORMAT)
 logger = logging.getLogger()
 
-IGNORE_PACKAGES = ['lego-git', 'linux-precision5530', 'aws-session-manager-plugin-jantman', 'brother-mfc-j5945dw']
+IGNORE_PACKAGES = [
+    'lego-git', 'linux-precision5530', 'aws-session-manager-plugin-jantman', 'brother-mfc-j5945dw',
+]
 
 
 class ArchRebuilder(object):
